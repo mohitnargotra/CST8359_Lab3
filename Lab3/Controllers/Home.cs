24b075ee97a3cf34ed8bba4811eb486ad4a867fc
Mohit <mohitn2804@gmail.com>
2022-10-09 17:44:39 -0400

    Initial commit

---
+++ Lab3/Controllers/Home.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Lab3.Controllers
+{
+    public class Home : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       // public IActionResult SongForm() => View();
+
+        [HttpGet]
+        public IActionResult SongForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            HttpContext.Session.SetString("numBottles", Request.Form["numBottles"]);
+
+            //HttpContext.Session.SetInt32("numBottles", Convert.ToInt32(Request.Form["numBottles"]));
+            // Console.WriteLine(Request.Form["numBottles"]);
+            // Console.WriteLine(HttpContext.Session.GetString("numBottles"));
+
+            // return View(HttpContext.Session.GetString("numBottles"));
+
+            // you will complete this
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        //public IActionResult DisplayPerson(Person person)
+       // {
+            // you will complete this
+       // }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string? EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        [BindProperty(Name = "pass")]
+        public string? Password
+        {
+            get;
+            set;
+        }
+
+        public string? Desc
+        {
+            get;
+            set;
+        }
+    }
+}
